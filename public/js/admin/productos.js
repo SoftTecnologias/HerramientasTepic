ce6a1e2aa7dd919ad922c56d1eb82fa58a135d46
necro213 <necro213a@gmail.com>
2017-07-19 16:45:11 -0600

    fix: Error de las Rutas

--- public/js/admin/productos.js
+++ public/js/admin/productos.js
@@ -16,7 +16,7 @@ $(function(){
         var categoria = $(this).val();
         if(categoria != '00'){
             $.ajax({
-                url:document.location.protocol+'//'+document.location.host+'/HerramientasTepic/public'+'/api/getsubcategoria/'+categoria,
+                url:document.location.protocol+'//'+document.location.host+'/api/getsubcategoria/'+categoria,
                 type:'GET'
             }).done(function(response){
                 if(response.code == 200){
@@ -40,7 +40,7 @@ $(function(){
         'scrollY':'600px',
         "processing": true,
         "serverSide": true,
-        "ajax": document.location.protocol+'//'+document.location.host+'/HerramientasTepic/public'+'/area/resource/productos',
+        "ajax": document.location.protocol+'//'+document.location.host+'/area/resource/productos',
         'createdRow':function(row,data,index){
             if(data.stock <= data.reorderpoint ){
               $('td', row).addClass("danger");
@@ -275,7 +275,7 @@ function productAction(){
 function newProduct(){
     var data = new FormData(document.getElementById("productForm"));
     $.ajax({
-        url:document.location.protocol+'//'+document.location.host+'/HerramientasTepic/public'+  +"/area/resource/productos",
+        url:document.location.protocol+'//'+document.location.host+  +"/area/resource/productos",
         type:"POST",
         data: data,
         contentType:false,
@@ -301,7 +301,7 @@ function updateProduct(id){
     $("#productid").val(id);
     var datos = new FormData(document.getElementById("productForm"));
     $.ajax({
-        url:document.location.protocol+'//'+document.location.host+'/HerramientasTepic/public'  +"/area/resource/productos/"+id,
+        url:document.location.protocol+'//'+document.location.host  +"/area/resource/productos/"+id,
         type:"POST",
         data: datos,
         contentType:false,
@@ -335,7 +335,7 @@ function deleteProduct(id){
         confirmButtonText: 'Si, deseo eliminarlo!',
         cancelButtonText: "Lo pensaré"
     }).then(function () {
-        ruta =document.location.protocol+'//'+document.location.host+'/HerramientasTepic/public'  +'/area/resource/productos/'+id;
+        ruta =document.location.protocol+'//'+document.location.host  +'/area/resource/productos/'+id;
         $.ajax({
             url:ruta,
             type:'delete',
@@ -368,7 +368,7 @@ function showProduct(productid, categoryid, code, currency, longdescription, bra
     $('#brandid').val(brandid);
     $('#categoryid').val(categoryid);
     $.ajax({
-        url: document.location.protocol+'//'+document.location.host+'/HerramientasTepic/public'+'/api/getsubcategoria/'+categoryid,
+        url: document.location.protocol+'//'+document.location.host+'/api/getsubcategoria/'+categoryid,
         type: 'GET'
     }).done(function (json) {
         if (json.code === 200) {
@@ -443,7 +443,7 @@ function verMiniatura(id) {
         confirmButtonText: 'Aceptar',
         cancelButtonText: "Cancelar"
     }).then(function () {
-        ruta =document.location.protocol+'//'+document.location.host+'/HerramientasTepic/public'  +'/area/resource/verproducto/'+id;
+        ruta =document.location.protocol+'//'+document.location.host  +'/area/resource/verproducto/'+id;
         if(checkbox.checked){
             $('#no').val(1);
         }else{
